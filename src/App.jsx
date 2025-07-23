ebde270d5384948de75aa70309e5ed230b092953
v011 <vijaykolla01@icloud.com>
2025-07-23 08:10:23 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          welcome to my app
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
